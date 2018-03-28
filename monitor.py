96ee567c586876b49f7bd5968a244474109032f0
sevenquarkoniums <sevenquarkoniums@gmail.com>
2018-03-28 05:06:25 +0000

    task 1 script finished.

---
+++ monitor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+'''
+'''
+outputName = 'monitorSingle.out'
+import time
+import subprocess
+
+with open(outputName, 'w') as out:
+    out.write('time,cpu-clock,context-switches,cpu-migrations,page-faults,user,nice,system,idle,iowait,irq,softirq,steal\n')
+
+while 1:
+    perf = subprocess.check_output('sudo perf stat -a sleep 1 2>&1', shell=True)
+    perfsplit = perf.split()
+    m1 = float(perfsplit[6].replace(',', ''))
+    m2 = int(perfsplit[13].replace(',', ''))
+    m3 = int(perfsplit[18].replace(',', ''))
+    m4 = int(perfsplit[23].replace(',', ''))
+    procstat = subprocess.check_output('cat /proc/stat 2>&1', shell=True)
+    procstatsplit = procstat.split()
+    user = int(procstatsplit[1])
+    nice = int(procstatsplit[2])
+    system = int(procstatsplit[3])
+    idle = int(procstatsplit[4])
+    iowait = int(procstatsplit[5])
+    irq = int(procstatsplit[6])
+    softirq = int(procstatsplit[7])
+    steal = int(procstatsplit[8])
+    t = time.time()
+    data = '%.1f,%.1f,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d' % (t, m1, m2, m3, m4, user, nice, system, idle, iowait, irq, softirq, steal)
+    print data
+    with open(outputName, 'a') as out:
+        out.write('%s\n' % data)
+
---
+++ singleRun.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+'''
+import time
+import subprocess
+
+with open('appSingle.out', 'w') as out:
+    out.write('run,thread,app,startTime,endTime\n')
+
+app_bin='/home/centos/benchmarks/parsec-3.0/bin/'
+for irun in range(10):
+    print 'run number: %d' % irun
+    for thread in [32,16,8,4,2,1]:
+        print 'thread number: %d' % thread
+        for app in ['dedup', 'swaptions']:
+            print 'Starting job ' + app
+            appStart = time.time()
+            if app == 'dedup':
+                cmd = '{}parsecmgmt -a run -i native -n {} -p {}'.format(app_bin, thread, app)
+            elif app == 'swaptions':
+                cmd = '{}parsecmgmt -a run -i native -n {} -p {} -c gcc-tbb'.format(app_bin, thread, app)
+            subprocess.call(cmd, shell=True)
+            appEnd = time.time()
+            with open('appSingle.out', 'a') as out:
+                out.write('%d,%d,%s,%.1f,%.1f\n' % (irun, thread, app, appStart, appEnd))
+
+print 'finished, and appSingle.out is updated.'
